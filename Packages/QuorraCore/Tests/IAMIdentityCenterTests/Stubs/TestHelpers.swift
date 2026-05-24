ad9aaec507b8906ba78531f316833e95aa758d35
AJ Beck <github@ajbeck.uk>
2026-05-24 11:56:55 +0100

    patch(test): focus IAM Identity Center coverage

--- Packages/QuorraCore/Tests/IAMIdentityCenterTests/Stubs/TestHelpers.swift
+++ Packages/QuorraCore/Tests/IAMIdentityCenterTests/Stubs/TestHelpers.swift
@@ -71,30 +71,3 @@ func makeVerification(
         interval: interval
     )
 }
-
-// MARK: - StubURLProtocol wire helpers
-
-/// Registers a successful `RegisterClient` response for the test's stubbed URLSession.
-func registerStub(
-    clientId: String = "test-client-id",
-    clientSecret: String = "test-client-secret"
-) throws {
-    try StubURLProtocol.registerSuccess(urlSubstring: "/client/register", json: [
-        "clientId": clientId,
-        "clientSecret": clientSecret,
-        "clientIdIssuedAt": Int(Date().timeIntervalSince1970),
-        "clientSecretExpiresAt": Int(Date().addingTimeInterval(90 * 24 * 60 * 60).timeIntervalSince1970),
-    ])
-}
-
-/// Registers a successful `StartDeviceAuthorization` response.
-func deviceAuthStub(expiresIn: Int = 600, interval: Int = 5) throws {
-    try StubURLProtocol.registerSuccess(urlSubstring: "/device_authorization", json: [
-        "deviceCode": "test-device-code",
-        "userCode": "ABCD-1234",
-        "verificationUri": "https://device.sso.us-east-1.amazonaws.com/",
-        "verificationUriComplete": "https://device.sso.us-east-1.amazonaws.com/?user_code=ABCD-1234",
-        "expiresIn": expiresIn,
-        "interval": interval,
-    ])
-}
